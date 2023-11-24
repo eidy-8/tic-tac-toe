94970582f35e9879f82163c29e1f210f75704b37
enrique <enriqueonaga@gmail.com>
2023-11-24 20:15:31 -0300

    develop the logic

--- script.js
+++ script.js
@@ -1,32 +1,136 @@
-const gameboard = { //gameboard status
-    board: [0,1,2,3,4,5,6,7,8]
+// Factory function for creating players
+const createPlayer = (name, marker) => {
+    return {name, marker};
 };
 
-const game = { //game flow
-    getPlayer: function(
-        playerOneName = "Player One",
-        playerTwoName = "Player Two"
-    ) {
-        const player = [
-            {
-                name: playerOneName,
-                marker: "O"
-            },
-            {
-                name: playerTwoName,
-                marker: "X"
-            }
-        ]
-        return player[1].name;
-    },
+// Factory function for creating the game board
+const createGameboard = () => { //gameboard status
+    const board = ["","","","","","","","",""];
 
-    playRound: function() {
-        let activePlayer = player[0];
-
-        const getActivePlayer = () => {
-            activePlayer = activePlayer === player[0] ? player[1] : player[0];
-        };
+    const printBoard = () => {
+        console.log("Current Board:");
+        console.log(board);
     }
-}
 
-console.log(game.getPlayer());
+    return {board, printBoard};
+};
+
+// Factory function for controlling the game flow
+const createGame = () => { 
+    let currentPlayer;
+    let gameActive = true;
+    const players = [createPlayer("Player1", "X"), createPlayer("Player2", "O")];
+    const gameboard = createGameboard();
+
+    const switchPlayer = () => {
+        currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    };
+
+    const makeMove = (position) => {
+        if (gameboard.board[position] === "" && gameActive) {
+            gameboard.board[position] = currentPlayer.marker;
+            gameboard.printBoard();
+            
+            if (checkWin(position)) {
+                console.log(`${currentPlayer.name} wins!`);
+                gameActive = false;
+            } else if (checkTie()) {
+                console.log("It\'s a tie!");
+                gameActive = false;
+            } else {
+                switchPlayer();
+            }
+        }
+    };
+
+    const checkWin = (position) => {
+        //check rows
+        if (
+            gameboard.board[0] === currentPlayer.marker &&
+            gameboard.board[1] === currentPlayer.marker &&
+            gameboard.board[2] === currentPlayer.marker
+        ) {
+            return true;
+        }
+
+        if (
+            gameboard.board[3] === currentPlayer.marker &&
+            gameboard.board[4] === currentPlayer.marker &&
+            gameboard.board[5] === currentPlayer.marker
+        ) {
+            return true;
+        }
+
+        if (
+            gameboard.board[6] === currentPlayer.marker &&
+            gameboard.board[7] === currentPlayer.marker &&
+            gameboard.board[8] === currentPlayer.marker
+        ) {
+            return true;
+        }
+
+        //check columns
+        if (
+            gameboard.board[0] === currentPlayer.marker &&
+            gameboard.board[3] === currentPlayer.marker &&
+            gameboard.board[6] === currentPlayer.marker
+        ) {
+            return true;
+        }
+
+        if (
+            gameboard.board[1] === currentPlayer.marker &&
+            gameboard.board[4] === currentPlayer.marker &&
+            gameboard.board[7] === currentPlayer.marker
+        ) {
+            return true;
+        }
+
+        if (
+            gameboard.board[2] === currentPlayer.marker &&
+            gameboard.board[5] === currentPlayer.marker &&
+            gameboard.board[8] === currentPlayer.marker
+        ) {
+            return true;
+        }
+
+        //check diagonals
+        if (
+            gameboard.board[0] === currentPlayer.marker &&
+            gameboard.board[4] === currentPlayer.marker &&
+            gameboard.board[8] === currentPlayer.marker
+        ) {
+            return true;
+        }
+
+        if (
+            gameboard.board[2] === currentPlayer.marker &&
+            gameboard.board[4] === currentPlayer.marker &&
+            gameboard.board[6] === currentPlayer.marker
+        ) {
+            return true;
+        }
+
+        return false;
+    };
+
+    const checkTie = () => {
+        if (gameboard.board.includes("") == false) {
+            return true;
+        }
+        return false;
+    };
+
+    const startGame = () => {
+        currentPlayer = players[Math.floor(Math.random() * 2)];
+        gameboard.printBoard();
+        console.log(`${currentPlayer.name} (${currentPlayer.marker}) goes first!`);
+    };
+
+    return {startGame, makeMove};
+};
+
+const game = createGame();
+game.startGame();
+
+//makeMove(0); => code to play the game in the console.
