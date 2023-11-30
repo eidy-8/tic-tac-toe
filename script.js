287b19302748ca164328e0d23e6cf73405a28939
enrique <enriqueonaga@gmail.com>
2023-11-29 23:15:28 -0300

    develop the logic

--- script.js
+++ script.js
@@ -1,41 +1,54 @@
 // Factory function for creating players
-const createPlayer = (name, marker) => {
-    return {name, marker};
+const createPlayer = (name, marker, score) => {
+    return {name, marker, score};
 };
 
 // Factory function for creating the game board
 const createGameboard = () => { //gameboard status
     const board = ["","","","","","","","",""];
 
-    const printBoard = () => {
-        console.log("Current Board:");
-        console.log(board);
-    }
-
-    return {board, printBoard};
+    return {board};
 };
 
 // Factory function for controlling the game flow
 const createGame = () => { 
     let currentPlayer;
     let gameActive = true;
-    const players = [createPlayer("Player1", "X"), createPlayer("Player2", "O")];
+    const players = [createPlayer("Player1", "X", 0), createPlayer("Player2", "O", 0)];
     const gameboard = createGameboard();
 
     const switchPlayer = () => {
+        let gameTextElement = document.getElementById("topTxt");
+
         currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+
+        gameTextElement.innerHTML = "";
+        gameTextElement.innerHTML = `${currentPlayer.name}'s turn (${currentPlayer.marker})`;
     };
 
     const makeMove = (position) => {
+        let gameTextElement = document.getElementById("topTxt");
+        let scoreElement = document.getElementById("score");
+
         if (gameboard.board[position] === "" && gameActive) {
             gameboard.board[position] = currentPlayer.marker;
-            gameboard.printBoard();
+
+            const cell = document.getElementById(`btn${position}`);
+            cell.innerHTML = `${currentPlayer.marker}`;
             
             if (checkWin(position)) {
-                console.log(`${currentPlayer.name} wins!`);
+                currentPlayer.score += 1;
+                gameTextElement.innerHTML = "";
+                scoreElement.innerHTML = `${players[0].name}'s Score: ${players[0].score} | ${players[1].name}'s Score: ${players[1].score}`;
+                gameTextElement.innerHTML = `${currentPlayer.name} wins!`;
+
+                currentPlayer = undefined;
+
                 gameActive = false;
             } else if (checkTie()) {
-                console.log("It\'s a tie!");
+                gameTextElement.innerHTML = "";
+                gameTextElement.innerHTML = "It\'s a tie!";
+
                 gameActive = false;
             } else {
                 switchPlayer();
@@ -122,15 +135,23 @@ const createGame = () => {
     };
 
     const startGame = () => {
-        currentPlayer = players[Math.floor(Math.random() * 2)];
-        gameboard.printBoard();
-        console.log(`${currentPlayer.name} (${currentPlayer.marker}) goes first!`);
+        let gameTextElement = document.createElement("p");
+        gameTextElement.id = "topTxt";
+        let displayElement = document.getElementById("display");
+        let scoreElement = document.getElementById("score");
+
+        if (currentPlayer == undefined) {
+            currentPlayer = players[Math.floor(Math.random() * 2)];
+
+            scoreElement.innerHTML = `${players[0].name}'s Score: ${players[0].score} | ${players[1].name}'s Score: ${players[1].score}`;
+            gameTextElement.innerHTML = `${currentPlayer.name} (${currentPlayer.marker}) goes first!`;
+            displayElement.appendChild(gameTextElement);
+        }
     };
 
     return {startGame, makeMove};
 };
 
 const game = createGame();
-game.startGame();
 
-//makeMove(0); => code to play the game in the console
+
